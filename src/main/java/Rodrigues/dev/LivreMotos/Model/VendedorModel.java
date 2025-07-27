60ff1a3a3740f623773c5fcc16c13e650856ac54
Victor Wesley <victor.infowb@gmail.com>
2025-07-27 18:48:45 -0300

    feat: Adiciona construtor e getters e setters para VendedorModel

--- src/main/java/Rodrigues/dev/LivreMotos/Model/VendedorModel.java
+++ src/main/java/Rodrigues/dev/LivreMotos/Model/VendedorModel.java
@@ -21,4 +21,42 @@ public class VendedorModel {
     @JsonIgnore
     private List<OrdemVendaModel> ordensVenda = new ArrayList<>();
 
+
+    public VendedorModel(String nome, String email, List<OrdemVendaModel> ordensVenda) {
+        this.nome = nome;
+        this.email = email;
+        this.ordensVenda = ordensVenda;
+    }
+
+    public Long getId() {
+        return Id;
+    }
+
+    public void setId(Long id) {
+        Id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public List<OrdemVendaModel> getOrdensVenda() {
+        return ordensVenda;
+    }
+
+    public void setOrdensVenda(List<OrdemVendaModel> ordensVenda) {
+        this.ordensVenda = ordensVenda;
+    }
 }
