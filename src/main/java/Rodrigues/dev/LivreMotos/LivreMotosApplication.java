4203b9054ae872a880e4aea27926b8223dc5b043
Victor Wesley <victor.infowb@gmail.com>
2025-07-27 16:34:56 -0300

    chore: Estrutura inicial Spring

---
+++ src/main/java/Rodrigues/dev/LivreMotos/LivreMotosApplication.java
@@ -0,0 +1,13 @@
+package Rodrigues.dev.LivreMotos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LivreMotosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LivreMotosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Rodrigues/dev/LivreMotos/LivreMotosApplicationTests.java
@@ -0,0 +1,13 @@
+package Rodrigues.dev.LivreMotos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LivreMotosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
