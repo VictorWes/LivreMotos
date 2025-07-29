92b22a53fe79eb30f527c7289ca88187096f9f95
Victor Wesley <victor.infowb@gmail.com>
2025-07-29 20:56:07 -0300

    feat: Adiciona a logica para criar clientes

--- src/main/java/Rodrigues/dev/LivreMotos/Service/ClienteService.java
+++ src/main/java/Rodrigues/dev/LivreMotos/Service/ClienteService.java
@@ -1,4 +1,28 @@
 package Rodrigues.dev.LivreMotos.Service;
 
+import Rodrigues.dev.LivreMotos.DTO.ClienteDTO;
+import Rodrigues.dev.LivreMotos.Mapper.ClienteMapper;
+import Rodrigues.dev.LivreMotos.Model.ClienteModel;
+import Rodrigues.dev.LivreMotos.Repository.ClienteRepository;
+import org.springframework.stereotype.Service;
+
+@Service
 public class ClienteService {
+
+    private final ClienteRepository clienteRepository;
+    private final ClienteMapper clienteMapper;
+
+    public ClienteService(ClienteRepository clienteRepository, ClienteMapper clienteMapper) {
+        this.clienteRepository = clienteRepository;
+        this.clienteMapper = clienteMapper;
+    }
+
+
+    public ClienteDTO criarCliente(ClienteDTO clienteDTO){
+        ClienteModel cliente = clienteMapper.toModel(clienteDTO);
+        cliente =clienteRepository.save(cliente);
+        return clienteMapper.toDTO(cliente);
+    }
+
+
 }
