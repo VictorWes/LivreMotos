6143b21f68eeef5b12e0a9a80ba90d5c8950fdb7
Victor Wesley <victor.infowb@gmail.com>
2025-07-31 21:54:27 -0300

    feat: Implementa CRUD para Vendedor

--- src/main/java/Rodrigues/dev/LivreMotos/Controller/VendedorController.java
+++ src/main/java/Rodrigues/dev/LivreMotos/Controller/VendedorController.java
@@ -1,4 +1,63 @@
 package Rodrigues.dev.LivreMotos.Controller;
 
+import Rodrigues.dev.LivreMotos.DTO.VendedorDTO;
+import Rodrigues.dev.LivreMotos.Service.VendedorService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RequestMapping("vendedor")
+@RestController
 public class VendedorController {
+
+    private final VendedorService vendedorService;
+
+    public VendedorController(VendedorService vendedorService) {
+        this.vendedorService = vendedorService;
+    }
+
+    @PatchMapping("/alterar/{id}")
+    public ResponseEntity<?> alterarDadosVendedor(@PathVariable Long id, @RequestBody VendedorDTO vendedorDTO){
+        VendedorDTO alterarVendedor = vendedorService.alterarDadosVendedor(id, vendedorDTO);
+        if (alterarVendedor != null){
+            return ResponseEntity.ok(alterarVendedor);
+        }else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("ID não localizada");
+        }
+    }
+
+    @PostMapping("/criar")
+    public ResponseEntity<?> criarVendedor(@RequestBody VendedorDTO vendedorDTO){
+        VendedorDTO novoVendedor = vendedorService.criarVendedor(vendedorDTO);
+        return ResponseEntity.ok(novoVendedor);
+    }
+
+    @GetMapping("/listar")
+    public ResponseEntity<List<VendedorDTO>> listarVendedores(){
+        List<VendedorDTO> vendedores = vendedorService.listarVendedor();
+        return ResponseEntity.ok(vendedores);
+    }
+
+    @GetMapping("/listar/{id}")
+    public ResponseEntity<?> listarVendedorId(@PathVariable Long id){
+        VendedorDTO vendedorId = vendedorService.listarVendedorId(id);
+        if (id != null){
+            return ResponseEntity.ok(vendedorId);
+        }else {
+        return  ResponseEntity.status(HttpStatus.NOT_FOUND).body("Id não localizado");
+        }
+    }
+
+    @DeleteMapping("/deletar/{id}")
+    public ResponseEntity<?> deletarVendedor(@PathVariable Long id){
+        VendedorDTO vendedorId = vendedorService.listarVendedorId(id);
+        if (vendedorId != null){
+            vendedorService.deletarVendedor(id);
+            return ResponseEntity.ok(vendedorId);
+        }else {
+           return ResponseEntity.status(HttpStatus.NOT_FOUND).body("ID não localziada");
+        }
+    }
 }
--- src/main/java/Rodrigues/dev/LivreMotos/Service/VendedorService.java
+++ src/main/java/Rodrigues/dev/LivreMotos/Service/VendedorService.java
@@ -1,8 +1,15 @@
 package Rodrigues.dev.LivreMotos.Service;
 
+import Rodrigues.dev.LivreMotos.DTO.VendedorDTO;
 import Rodrigues.dev.LivreMotos.Mapper.VendedorMapper;
+import Rodrigues.dev.LivreMotos.Model.VendedorModel;
 import Rodrigues.dev.LivreMotos.Repository.VendedorRepository;
+import Rodrigues.dev.LivreMotos.Utils.MergeUtils;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class VendedorService {
@@ -13,5 +20,31 @@ public class VendedorService {
     public VendedorService(VendedorMapper vendedorMapper, VendedorRepository vendedorRepository) {
         this.vendedorMapper = vendedorMapper;
         this.vendedorRepository = vendedorRepository;
+    }
+
+    public VendedorDTO criarVendedor(VendedorDTO vendedorDTO){
+        VendedorModel vendedorCriar =  vendedorMapper.toModel(vendedorDTO);
+        vendedorCriar = vendedorRepository.save(vendedorCriar);
+        return vendedorMapper.toDTO(vendedorCriar);
+    }
+
+    public List<VendedorDTO> listarVendedor(){
+        List<VendedorModel> vendedorLista = vendedorRepository.findAll();
+        return vendedorLista.stream().map(vendedorMapper::toDTO)
+                .collect(Collectors.toList());
+    }
+    public VendedorDTO listarVendedorId(Long id){
+        Optional<VendedorModel> vendedorId = vendedorRepository.findById(id);
+        return vendedorId.map(vendedorMapper::toDTO).orElse(null);
+    }
+
+    public VendedorDTO alterarDadosVendedor(Long id, VendedorDTO vendedorDTO){
+        VendedorModel alterarVendedor = vendedorRepository.findById(id).orElseThrow(() -> new RuntimeException("ID não localizada"));
+        MergeUtils.copiaCampos(alterarVendedor, vendedorDTO);
+        vendedorRepository.save(alterarVendedor);
+        return vendedorMapper.toDTO(alterarVendedor);
+    }
+    public void deletarVendedor(Long id){
+        vendedorRepository.deleteById(id);
     }
 }
